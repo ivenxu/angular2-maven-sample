b0b21b45001ed65c6617ca1998c232a2f8a761fa
Iven <ivenxu@gmail.com>
2016-03-08 09:36:33 +0800

    use bootstrap style

--- src/main/front/app/js/app.component.ts
+++ src/main/front/app/js/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>My First Angular 2 App</h1>
+    <p class="bg-primary">foo bar</p>`
 })
 export class AppComponent { }
